b3ce576b9262e017b9d3c83b4e2232966be88147
Karol Będkowski <karol.bedkowski@gmail.com>
2025-11-09 09:30:26 +0100

    fix column types in settings

--- internal/db/migrations/20251108114410_settings_fix.sql
+++ internal/db/migrations/20251108114410_settings_fix.sql
@@ -3,9 +3,9 @@
 
 DROP INDEX settings_idx;
 
-ALTER TABLE settings ADD COLUMN podcast_id REFERENCES podcasts(id) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE settings ADD COLUMN episode_id REFERENCES episodes(id) ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE settings ADD COLUMN device_id REFERENCES devices(id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE settings ADD COLUMN podcast_id INT REFERENCES podcasts(id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE settings ADD COLUMN episode_id INT REFERENCES episodes(id) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE settings ADD COLUMN device_id INT REFERENCES devices(id) ON DELETE CASCADE ON UPDATE CASCADE;
 
 CREATE UNIQUE INDEX settings_idx ON settings(user_id, scope, podcast_id, episode_id, device_id, key);
 
