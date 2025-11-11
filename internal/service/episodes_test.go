432d59bfef984e1e032e1fce3fe5a6126e541cd5
Karol Będkowski <karol.bedkowski@gmail.com>
2025-11-11 19:10:46 +0100

    tests: add more test for episodes service

--- internal/service/episodes_test.go
+++ internal/service/episodes_test.go
@@ -26,6 +26,7 @@ func TestEpisodesServiceSave(t *testing.T) {
 	_ = prepareTestUser(ctx, t, i, "user1")
 	_ = prepareTestUser(ctx, t, i, "user2")
 	prepareTestDevice(ctx, t, i, "user1", "dev1")
+	prepareTestDevice(ctx, t, i, "user1", "dev2")
 	prepareTestSub(
 		ctx,
 		t,
@@ -37,41 +38,7 @@ func TestEpisodesServiceSave(t *testing.T) {
 		"http://example.com/p3",
 	)
 
-	started, position, total := 10, 20, 300
-	episodeActions := []model.Episode{
-		{
-			Podcast:   "http://example.com/p1",
-			Episode:   "http://example.com/p1/ep1",
-			Device:    "dev1",
-			Action:    "download",
-			Timestamp: time.Date(2025, 1, 2, 3, 4, 5, 0, time.UTC),
-		},
-		{
-			Podcast:   "http://example.com/p1",
-			Episode:   "http://example.com/p1/ep1",
-			Device:    "dev1",
-			Timestamp: time.Date(2025, 1, 3, 3, 4, 5, 0, time.UTC),
-			Action:    "play",
-			Started:   &started,
-			Position:  &position,
-			Total:     &total,
-		},
-		{
-			Podcast:   "http://example.com/p1",
-			Episode:   "http://example.com/p1/ep2",
-			Device:    "dev1",
-			Action:    "download",
-			Timestamp: time.Date(2025, 1, 4, 3, 4, 5, 0, time.UTC),
-		},
-		{
-			Podcast:   "http://example.com/p2",
-			Episode:   "http://example.com/p2/ep1",
-			Device:    "dev2",
-			Action:    "delete",
-			Timestamp: time.Date(2025, 1, 5, 3, 4, 5, 0, time.UTC),
-		},
-	}
-
+	episodeActions := prepareEpisodes()
 	err := episodesSrv.SaveEpisodesActions(ctx, "user1", episodeActions...)
 	assert.NoErr(t, err)
 
@@ -104,4 +71,199 @@ func TestEpisodesServiceSave(t *testing.T) {
 	assert.Equal(t, episodes[2].Started, nil)
 	assert.Equal(t, episodes[2].Position, nil)
 	assert.Equal(t, episodes[2].Total, nil)
+
+	// only one podcast, device should be ignored
+	episodes, err = episodesSrv.GetPodcastEpisodes(ctx, "user1", "http://example.com/p1", "dev2")
+	assert.NoErr(t, err)
+	assert.Equal(t, len(episodes), 2)
+	assert.Equal(t, episodes[0].Podcast, "http://example.com/p1")
+	assert.Equal(t, episodes[0].Episode, "http://example.com/p1/ep1")
+	assert.Equal(t, episodes[0].Action, "play")
+	assert.Equal(t, episodes[0].Device, "dev1")
+	assert.Equal(t, episodes[0].Timestamp, time.Date(2025, 1, 3, 3, 4, 5, 0, time.UTC))
+	assert.Equal(t, *episodes[0].Started, 10)
+	assert.Equal(t, *episodes[0].Position, 20)
+	assert.Equal(t, *episodes[0].Total, 300)
+	assert.Equal(t, episodes[1].Podcast, "http://example.com/p1")
+	assert.Equal(t, episodes[1].Episode, "http://example.com/p1/ep2")
+	assert.Equal(t, episodes[1].Device, "dev1")
+	assert.Equal(t, episodes[1].Action, "download")
+	assert.Equal(t, episodes[1].Timestamp, time.Date(2025, 1, 4, 3, 4, 5, 0, time.UTC))
+	assert.Equal(t, episodes[1].Started, nil)
+	assert.Equal(t, episodes[1].Position, nil)
+	assert.Equal(t, episodes[1].Total, nil)
+}
+
+func TestEpisodesServiceActions(t *testing.T) {
+	ctx := context.Background()
+	i := prepareTests(ctx, t)
+	ctx = log.Logger.WithContext(ctx)
+	episodesSrv := do.MustInvoke[*Episodes](i)
+	_ = prepareTestUser(ctx, t, i, "user1")
+	_ = prepareTestUser(ctx, t, i, "user2")
+	prepareTestDevice(ctx, t, i, "user1", "dev1")
+	prepareTestDevice(ctx, t, i, "user1", "dev2")
+	prepareTestSub(
+		ctx,
+		t,
+		i,
+		"user1",
+		"dev1",
+		"http://example.com/p1",
+		"http://example.com/p2",
+		"http://example.com/p3",
+	)
+
+	episodeActions := prepareEpisodes()
+	err := episodesSrv.SaveEpisodesActions(ctx, "user1", episodeActions...)
+	assert.NoErr(t, err)
+
+	// get all
+	episodes, err := episodesSrv.GetEpisodesActions(ctx, "user1", "", "", time.Time{}, false)
+	assert.NoErr(t, err)
+	assert.Equal(t, len(episodes), 4)
+	assert.Equal(t, episodes[0], episodeActions[0])
+	assert.Equal(t, episodes[1], episodeActions[1])
+	assert.Equal(t, episodes[2], episodeActions[2])
+	assert.Equal(t, episodes[3], episodeActions[3])
+
+	// get since 2025-01-04
+	episodes, err = episodesSrv.GetEpisodesActions(ctx, "user1", "", "",
+		time.Date(2025, 1, 4, 0, 0, 0, 0, time.UTC), false,
+	)
+	assert.NoErr(t, err)
+	assert.Equal(t, len(episodes), 2)
+	assert.Equal(t, episodes[0], episodeActions[2])
+	assert.Equal(t, episodes[1], episodeActions[3])
+
+	// get all aggregated (last action)
+	episodes, err = episodesSrv.GetEpisodesActions(ctx, "user1", "", "", time.Time{}, true)
+	assert.NoErr(t, err)
+	assert.Equal(t, len(episodes), 3)
+	assert.Equal(t, episodes[0], episodeActions[1])
+	assert.Equal(t, episodes[1], episodeActions[2])
+	assert.Equal(t, episodes[2], episodeActions[3])
+
+	// get one podcase aggregated; device should be ignored
+	episodes, err = episodesSrv.GetEpisodesActions(ctx, "user1", "http://example.com/p1", "dev2", time.Time{}, false)
+	assert.NoErr(t, err)
+	assert.Equal(t, len(episodes), 3)
+	assert.Equal(t, episodes[0], episodeActions[0])
+	assert.Equal(t, episodes[1], episodeActions[1])
+	assert.Equal(t, episodes[2], episodeActions[2])
+
+	// get one podcase aggregated; device should be ignored; aggregated
+	episodes, err = episodesSrv.GetEpisodesActions(ctx, "user1", "http://example.com/p1", "dev2", time.Time{}, true)
+	assert.NoErr(t, err)
+	assert.Equal(t, len(episodes), 2)
+	assert.Equal(t, episodes[0], episodeActions[1])
+	assert.Equal(t, episodes[1], episodeActions[2])
+}
+
+func TestEpisodesServiceUpdates(t *testing.T) {
+	ctx := context.Background()
+	i := prepareTests(ctx, t)
+	ctx = log.Logger.WithContext(ctx)
+	episodesSrv := do.MustInvoke[*Episodes](i)
+	_ = prepareTestUser(ctx, t, i, "user1")
+	_ = prepareTestUser(ctx, t, i, "user2")
+	prepareTestDevice(ctx, t, i, "user1", "dev1")
+	prepareTestDevice(ctx, t, i, "user1", "dev2")
+	prepareTestSub(
+		ctx,
+		t,
+		i,
+		"user1",
+		"dev1",
+		"http://example.com/p1",
+		"http://example.com/p2",
+		"http://example.com/p3",
+	)
+
+	episodeActions := prepareEpisodes()
+	err := episodesSrv.SaveEpisodesActions(ctx, "user1", episodeActions...)
+	assert.NoErr(t, err)
+
+	// without device, no include actions (last action only)
+	updates, err := episodesSrv.GetEpisodesUpdates(ctx, "user1", "", time.Time{}, false)
+	assert.NoErr(t, err)
+	assert.Equal(t, len(updates), 3)
+	assert.Equal(t, updates[0].URL, episodeActions[1].Episode)
+	assert.Equal(t, updates[0].PodcastURL, episodeActions[1].Podcast)
+	assert.Equal(t, updates[0].Status, episodeActions[1].Action)
+	assert.Equal(t, updates[0].Episode, nil)
+	assert.Equal(t, updates[1].URL, episodeActions[2].Episode)
+	assert.Equal(t, updates[1].PodcastURL, episodeActions[2].Podcast)
+	assert.Equal(t, updates[1].Status, episodeActions[2].Action)
+	assert.Equal(t, updates[1].Episode, nil)
+	assert.Equal(t, updates[2].URL, episodeActions[3].Episode)
+	assert.Equal(t, updates[2].PodcastURL, episodeActions[3].Podcast)
+	assert.Equal(t, updates[2].Status, episodeActions[3].Action)
+	assert.Equal(t, updates[2].Episode, nil)
+
+	// without device, include action
+	updates, err = episodesSrv.GetEpisodesUpdates(ctx, "user1", "",
+		time.Date(2025, 1, 4, 0, 0, 0, 0, time.UTC), true)
+	assert.NoErr(t, err)
+	assert.Equal(t, len(updates), 2)
+	assert.Equal(t, updates[0].URL, episodeActions[2].Episode)
+	assert.Equal(t, updates[0].PodcastURL, episodeActions[2].Podcast)
+	assert.Equal(t, updates[0].Status, episodeActions[2].Action)
+	assert.Equal(t, *updates[0].Episode, episodeActions[2])
+	assert.Equal(t, updates[1].URL, episodeActions[3].Episode)
+	assert.Equal(t, updates[1].PodcastURL, episodeActions[3].Podcast)
+	assert.Equal(t, updates[1].Status, episodeActions[3].Action)
+	assert.Equal(t, *updates[1].Episode, episodeActions[3])
+
+	// with device (should return entries other that with dev1), include action
+	updates, err = episodesSrv.GetEpisodesUpdates(ctx, "user1", "dev2",
+		time.Date(2025, 1, 1, 0, 0, 0, 0, time.UTC), true)
+	assert.NoErr(t, err)
+	assert.Equal(t, len(updates), 2)
+	assert.Equal(t, updates[0].URL, episodeActions[1].Episode)
+	assert.Equal(t, updates[0].PodcastURL, episodeActions[1].Podcast)
+	assert.Equal(t, updates[0].Status, episodeActions[1].Action)
+	assert.Equal(t, *updates[0].Episode, episodeActions[1])
+	assert.Equal(t, updates[1].URL, episodeActions[2].Episode)
+	assert.Equal(t, updates[1].PodcastURL, episodeActions[2].Podcast)
+	assert.Equal(t, updates[1].Status, episodeActions[2].Action)
+	assert.Equal(t, *updates[1].Episode, episodeActions[2])
+}
+
+func prepareEpisodes() []model.Episode {
+	started, position, total := 10, 20, 300
+
+	return []model.Episode{
+		{
+			Podcast:   "http://example.com/p1",
+			Episode:   "http://example.com/p1/ep1",
+			Device:    "dev1",
+			Action:    "download",
+			Timestamp: time.Date(2025, 1, 2, 3, 4, 5, 0, time.UTC),
+		},
+		{
+			Podcast:   "http://example.com/p1",
+			Episode:   "http://example.com/p1/ep1",
+			Device:    "dev1",
+			Timestamp: time.Date(2025, 1, 3, 3, 4, 5, 0, time.UTC),
+			Action:    "play",
+			Started:   &started,
+			Position:  &position,
+			Total:     &total,
+		},
+		{
+			Podcast:   "http://example.com/p1",
+			Episode:   "http://example.com/p1/ep2",
+			Device:    "dev1",
+			Action:    "download",
+			Timestamp: time.Date(2025, 1, 4, 3, 4, 5, 0, time.UTC),
+		},
+		{
+			Podcast:   "http://example.com/p2",
+			Episode:   "http://example.com/p2/ep1",
+			Device:    "dev2",
+			Action:    "delete",
+			Timestamp: time.Date(2025, 1, 5, 3, 4, 5, 0, time.UTC),
+		},
+	}
 }
