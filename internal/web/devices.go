ff7f13602ecbfcf044f18d877934713090492b1f
Karol Będkowski <karol.bedkowski@gmail.com>
2025-11-25 16:58:09 +0100

    feat(web): add delete device

--- internal/web/devices.go
+++ internal/web/devices.go
@@ -16,6 +16,7 @@ import (
 	"github.com/samber/do/v2"
 	"gitlab.com/kabes/go-gpo/internal"
 	"gitlab.com/kabes/go-gpo/internal/aerr"
+	"gitlab.com/kabes/go-gpo/internal/command"
 	"gitlab.com/kabes/go-gpo/internal/model"
 	"gitlab.com/kabes/go-gpo/internal/query"
 	"gitlab.com/kabes/go-gpo/internal/server/srvsupport"
@@ -37,6 +38,8 @@ func newDevicePages(i do.Injector) (devicePages, error) {
 func (d devicePages) Routes() *chi.Mux {
 	r := chi.NewRouter()
 	r.Get(`/`, srvsupport.Wrap(d.list))
+	r.Get(`/{devicename:[\w.-]+}/delete`, srvsupport.Wrap(d.deleteGet))
+	r.Post(`/{devicename:[\w.-]+}/delete`, srvsupport.Wrap(d.deletePost))
 
 	return r
 }
@@ -62,4 +65,49 @@ func (d devicePages) list(ctx context.Context, w http.ResponseWriter, r *http.Re
 		logger.Error().Err(err).Msg("execute template error")
 		srvsupport.WriteError(w, r, http.StatusInternalServerError, "")
 	}
+}
+
+//nolint:revive
+func (d devicePages) deleteGet(ctx context.Context, w http.ResponseWriter, r *http.Request, logger *zerolog.Logger) {
+	devicename := chi.URLParam(r, "devicename")
+	if devicename == "" {
+		srvsupport.WriteError(w, r, http.StatusBadRequest, "")
+
+		return
+	}
+
+	data := struct {
+		DeviceName string
+	}{
+		DeviceName: devicename,
+	}
+
+	if err := d.template.executeTemplate(w, "device_delete.tmpl", &data); err != nil {
+		logger.Error().Err(err).Msg("execute template error")
+		srvsupport.WriteError(w, r, http.StatusInternalServerError, "")
+	}
+}
+
+func (d devicePages) deletePost(ctx context.Context, w http.ResponseWriter, r *http.Request, logger *zerolog.Logger) {
+	devicename := chi.URLParam(r, "devicename")
+	if devicename == "" {
+		srvsupport.WriteError(w, r, http.StatusBadRequest, "")
+
+		return
+	}
+
+	cmd := command.DeleteDeviceCmd{
+		UserName:   internal.ContextUser(ctx),
+		DeviceName: devicename,
+	}
+
+	err := d.deviceSrv.DeleteDevice(ctx, &cmd)
+	if err != nil {
+		srvsupport.CheckAndWriteError(w, r, err)
+		logger.WithLevel(aerr.LogLevelForError(err)).Err(err).Interface("cmd", &cmd).Msg("delete device error")
+
+		return
+	}
+
+	d.list(ctx, w, r, logger)
 }
